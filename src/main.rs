eea2a9b64dd1eca004fb0297d8d5255f0fd067ee
Mateusz Czapliński <czapkofan@gmail.com>
2025-04-11 01:44:25 +0200

    start using xflags

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
+    let flags = flags::Rumbac::from_env_or_exit();
+    println!("{flags:?}");
+
     let ports = serialport::available_ports().expect("failed to read serial ports");
     println!("Found {} serial ports.", ports.len());
     for p in ports {
         println!("port: {p:?}");
+    }
+}
+
+mod flags {
+    xflags::xflags! {
+        cmd rumbac {
+            default cmd list { }
+            cmd info {
+                required -p,--port port: String
+            }
+        }
     }
 }
